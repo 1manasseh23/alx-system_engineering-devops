a5618621c0c685ab3fda280e6629c05d882caee7
1manasseh23 <cletavivan@gmail.com>
2024-04-19 05:51:00 -0700

    only me

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,20 @@
-#!/usr/bin/env bash
-# Install Nginx package
+#setup nginx
 
-package { 'nginx':
-  ensure => installed,
+package {
+    'nginx':
+    ensure => installed,
 }
 
-# Configure Nginx server
-class { 'nginx':
-  listen_port => 80,
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
 }
 
-# Create a custom 404 page
-file { '/var/www/html/error404.html':
-  ensure  => present,
-  content => '<!DOCTYPE html>
-<html>
-<head>
-    <title>404 Not Found</title>
-</head>
-<body>
-    <h1>Ceci n\'est pas une page</h1>
-    <p>Sorry, the requested page could not be found.</p>
-</body>
-</html>',
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
 }
 
-# Set up the default server block
-nginx::resource::server { 'default':
-  listen_port => 80,
-  server_name => '_',
-  location    => {
-    '/' => {
-      try_files => '$uri $uri/ /error404.html', # Custom 404 page
-    },
-  },
-  error_page  => '404 /error404.html', # 301 redirect for /redirect_me
+service {'nginx':
+    ensure => running,
 }
