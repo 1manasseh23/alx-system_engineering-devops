5df46d175f346345bc520d3409581b3ad2da144c
1manasseh23 <cletavivan@gmail.com>
2024-04-22 16:56:18 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,14 +5,13 @@ First line: Employee EMPLOYEE_NAME is done with tasks
 (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
     :EMPLOYEE_NAME: name of the employee
     :NUMBER_OF_DONE_TASKS: number of completed tasks
-        :TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum
-        of completed and non-completed tasks
+    :TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum
+    of completed and non-completed tasks
 '''
 import requests
 from sys import argv
 
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     if len(argv) > 1:
         user = argv[1]
         url = "https://jsonplaceholder.typicode.com/"
