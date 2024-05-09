2c1e066bfe5b32456eefe39aa9e3ec5ded7d611e
1manasseh23 <cletavin@gmail.com>
2024-05-09 15:44:54 +0100

    How many subs?

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-import requests
+"""function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit."""
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit.
-    If not a valid subreddit, prints None.
-    """
-    headers = {'User-Agent': 'MyApp/0.0.1'}
+    """function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+    import requests
+    import sys
 
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     try:
-        response = requests.get(
-                f'https://www.reddit.com/r/{subreddit}/hot.json',
-                headers=headers)
-
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
         if response.status_code == 200:
-            data = response.json()
-
-            if 'data' in data and 'children' in data['data']:
-                for post in data['data']['children'][:10]:
-                    print(post['data']['title'])
-            else:
-                print(None)
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
         else:
-            print(None)
-    except (requests.exceptions.RequestException, KeyError):
-        print(None)
+            print("None")
+    except Exception:
+        print("None")
