29e2d230ca56cc3c2c012fdeed843ef8c855fc97
1manasseh23 <cletavin@gmail.com>
2024-05-09 13:39:36 +0100

    upt

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,5 @@
-# Ensure the necessary file or directory exists
-
-file { '/path/to/missing/file':
-  ensure => file,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0644',
-}
-
-# Update the Apache configuration
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => template('apache/000-default.conf.erb'),
-  notify  => Service['apache2'],
-}
-
-# Restart the Apache service
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  hasstatus  => true,
-}
-
+# puppet code to fix WordPress webstack bug
+exec { 'debugging fix':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/bin'
+  }
