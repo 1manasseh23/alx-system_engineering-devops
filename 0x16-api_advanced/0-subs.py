5a8505048299e91f73bd408354f2388d532b475a
1manasseh23 <cletavin@gmail.com>
2024-05-09 15:33:01 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-""" import the request library """
+"""
+number of subscribers for a given subreddit
+"""
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
-    # Set a custom User-Agent to avoid rate limiting
-    headers = {'User-Agent': 'MyApp/0.0.1'}
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        # Make a GET request to the Reddit API for the given subreddit
-        response = requests.get(
-                f'https://www.reddit.com/r/{subreddit}/about.json',
-                headers=headers)
+        return results.get('data').get('subscribers')
 
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response and return the number of subscribers
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # If the request was not successful, return 0
-            return 0
-    except (requests.exceptions.RequestException, KeyError):
-        # If there was an error making the request or parsing the
-        # response, return 0
+    except Exception:
         return 0
