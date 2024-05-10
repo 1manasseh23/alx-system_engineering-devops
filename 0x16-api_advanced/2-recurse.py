c2a42e8b40f504a70c5bfe1f83295de6d4b68b13
1manasseh23 <cletavivan@gmail.com>
2024-05-10 02:07:54 +0100

    How many subs?

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API and returns
- a list containing the titles of all hot articles for a given subreddit. """
+"""import praw and  sys"""
 
-import requests
+import praw
+import sys
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], after=None):
+    """with your actual Reddit API credentials"""
+    reddit = praw.Reddit(
+        client_id='YOUR_CLIENT_ID',
+        client_secret='YOUR_CLIENT_SECRET',
+        user_agent='YOUR_USER_AGENT'
+    )
+
+    try:
+        subreddit_obj = reddit.subreddit(subreddit)
+        hot_posts = subreddit_obj.hot(limit=100, params={'after': after})
+
+        for post in hot_posts:
+            hot_list.append(post.title)
+
+        after = hot_list[-1]
+
+        return recurse(subreddit, hot_list, after)
+    
+    except Exception as e:
+        print("An error occurred:", e)
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
