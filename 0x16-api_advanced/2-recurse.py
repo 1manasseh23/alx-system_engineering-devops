4113ce126b80b03c6761b795bee2ea3f7eabd135
1manasseh23 <cletavivan@gmail.com>
2024-05-10 02:09:27 +0100

    How many subs?

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-"""import praw and  sys"""
-
+"""
+This a function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a
+given subreddit
+"""
 import praw
-import sys
+import prawcore
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """with your actual Reddit API credentials"""
+def recurse(subreddit, hot_list=None, after=None):
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    If not a valid subreddit, prints None
+    """
+    if hot_list is None:
+        hot_list = []
     reddit = praw.Reddit(
-        client_id='YOUR_CLIENT_ID',
-        client_secret='YOUR_CLIENT_SECRET',
-        user_agent='YOUR_USER_AGENT'
-    )
-
+            client_id='YOUR_CLIENT_ID',
+            client_secret='YOUR_CLIENT_SECRET',
+            user_agent='YOUR_USER_AGENT')
     try:
         subreddit_obj = reddit.subreddit(subreddit)
-        hot_posts = subreddit_obj.hot(limit=100, params={'after': after})
-
-        for post in hot_posts:
-            hot_list.append(post.title)
-
-        after = hot_list[-1]
-
-        return recurse(subreddit, hot_list, after)
-    
-    except Exception as e:
-        print("An error occurred:", e)
+        hot_articles = subreddit_obj.hot(limit=100, params={'after': after})
+        for article in hot_articles:
+            hot_list.append(article.title)
+        if hot_articles._listing:
+            return recurse(
+                    subreddit,
+                    hot_list,
+                    hot_articles._listing[-1].fullname)
+        else:
+            if hot_list:
+                return hot_list
+            else:
+                return None
+    except prawcore.exceptions.ResponseException as e:
         return None
