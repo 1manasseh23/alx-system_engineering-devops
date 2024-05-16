087c873696bbe28aa09d0e9766f0517e76f62ef5
1manasseh23 <cletavivan@gmail.com>
2024-05-16 17:48:57 +0000

    u

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,9 +2,9 @@
 
 # Define the command to change the user's file descriptor limit
 exec { 'change-os-configuration-for-holberton-user':
-    command => '/bin/bash -c "echo \"holberton hard nofile 4096\" >> /etc/security/limits.conf && ulimit -n 4096"',
+    command => '/bin/bash -c "echo \"holberton soft nofile 4096\nholberton hard nofile 4096\" >> /etc/security/limits.conf && ulimit -n 4096"',
     path    => ['/bin', '/usr/bin'],
-    unless  => '/bin/grep -q "holberton hard nofile 4096" /etc/security/limits.conf',
+    unless  => '/bin/grep -q "holberton soft nofile 4096" /etc/security/limits.conf && /bin/grep -q "holberton hard nofile 4096" /etc/security/limits.conf',
 }
 
 # Define a notify resource to print a message after the configuration is applied
